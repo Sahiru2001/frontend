5d78ddfcf68454ed16418df274e6c09ff16bfc01
Sahiru2001 <sahirusandeepa52@gmail.com>
2025-07-25 08:21:12 +0530

    Clean up environment and configuration files by removing commented-out code

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
 
-//968475863032-l22av7m9fp46rcusic6sfpvolmq4dq6g.apps.googleusercontent.com
-//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
+
