057584783be1df2b8d0628cb765e8170e723d95b
Sahiru2001 <sahirusandeepa52@gmail.com>
2025-07-25 08:14:15 +0530

    Implement Google OAuth login and add password reset functionality

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,16 +2,40 @@ import {useState} from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { GrGoogle } from "react-icons/gr";
+import { useGoogleLogin } from '@react-oauth/google';
 export default function LoginPage() {
 
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
     const navigate = useNavigate();
 
+    const googleLogin = useGoogleLogin({
+        onSuccess:(response)=>{
+            const accessToken = response.access_token;
+            axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login/google", {
+        
+                accessToken: accessToken
+            }).then((response)=>{
+                toast.success("Login Successful");
+                const token = response.data.token;
+                localStorage.setItem("token", token);
+                if(response.data.role === "admin"){
+                    navigate("/admin/")
+                }
+                else{
+                    navigate("/")
+                }
+                
+            })
+        }
+    })
+
+
+
 
     async function handleLogin() {
-        console.log(email)
-        console.log(password)
+
         try{
         const response = await axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login", {
             email: email,
@@ -38,7 +62,7 @@ export default function LoginPage() {
         <div className = "w-full h-screen bg-[url('login.jpg')] bg-center bg-cover flex justify-center items-center">
                 <div 
                     className = "w-[50%] h-full"></div>
-                <div className = "w-[50%] h-full flex flex justify-center items-center">
+                <div className = "w-[50%] h-full flex justify-center items-center">
                     <div className = "w-[500px] h-[600px] backdrop-blur-md rounded-[20px] shadow-xl flex flex-col justify-center items-center">
                         <input
                         
@@ -62,8 +86,15 @@ export default function LoginPage() {
 
                     value={password}
                         
-                        placeholder = "    Enter your Password" type ="password" className = "w-[300px] h-[50px] border border-[#e9e9e9] rounded-[20px] my-[20px]"/>
+                        placeholder = "Enter your Password" type ="password" className = "w-[300px] h-[50px] border border-[#e9e9e9] rounded-[20px] my-[20px]"/>
                         <button onClick = {handleLogin} className = "w-[300px] h-[50px] bg-[#e5c3c4] rounded-[20px] text-[20px] my-[20px] font-bold text-black cursor-pointer">Login</button>
+                            <button onClick={googleLogin} className="w-[250px] cursor-pointer h-[50px] flex justify-center items-center bg-white rounded-[20px] my-[20px] text-[20px] font-bold">
+                                <GrGoogle className="text-xl text-black-500 mx-2 cursor-pointer hover:text-gray-100"/>
+                                <span className="text-gray-600 text-xl font-semibold">Log in with Google</span>
+                            </button>
+                         
+                    
+                    
                     </div>
                     </div>
                     </div>
--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
+
+//968475863032-l22av7m9fp46rcusic6sfpvolmq4dq6g.apps.googleusercontent.com
+//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
