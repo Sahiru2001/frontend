6fef265c2a9a717dac2359cbe37f29cdee14519d
Sahiru2001 <sahirusandeepa52@gmail.com>
2025-07-27 10:19:48 +0530

    Implement Google login functionality and create password reset page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,79 +1,101 @@
-import {useState} from "react";
-import axios from "axios";
-import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
-export default function LoginPage() {
+import { useGoogleLogin } from "@react-oauth/google"
+import axios from "axios"
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { GrGoogle } from "react-icons/gr"
+import { useNavigate } from "react-router-dom"
 
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const navigate = useNavigate();
+export default function LoginPage(){
+
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const googleLogin  = useGoogleLogin({
+        onSuccess: (response)=>{
+            const accessToken = response.access_token
+            axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login/google", {
+                accessToken: accessToken
+            }).then((response)=>{
+                toast.success("Login Successful")
+                const token = response.data.token
+                localStorage.setItem("token", token)
+                if(response.data.role === "admin"){
+                    navigate("/admin/")
+                }
+                else{
+                    navigate("/")
+                }
+            })
+        }
+    })
 
 
-    async function handleLogin() {
-        console.log(email)
-        console.log(password)
+    async function handleLogin(){
         try{
-        const response = await axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login", {
-            email: email,
-            password: password
-        })
-      toast.success("Login Successful");
-        console.log(response.data);
-        localStorage.setItem("token", response.data.token);
+            const response = await axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login" , {
+                email:email,
+                password:password
+            })
+            //alert("Login Successful")
+            toast.success("Login Successful")
+            console.log(response.data)
+            localStorage.setItem("token",response.data.token)
 
-        if(response.data.role === "admin"){
-             navigate("/admin/")
-        }
-        else{
-            navigate("/")
+            if(response.data.role === "admin"){
+                navigate("/admin/")
+            }else{
+                navigate("/")
+            }
+
+            
+        }catch(e){
+            //alert(e.response.data.message)
+            toast.error(e.response.data.message)
         }
         
 
 
-    }catch (e){
-        toast.error(e.response.data.message);
-    }}
-    
-        return (
-        <div className = "w-full h-screen bg-[url('login.jpg')] bg-center bg-cover flex justify-center items-center">
-                <div 
-                    className = "w-[50%] h-full"></div>
-                <div className = "w-[50%] h-full flex flex justify-center items-center">
-                    <div className = "w-[500px] h-[600px] backdrop-blur-md rounded-[20px] shadow-xl flex flex-col justify-center items-center">
-                        <input
-                        
-                        onChange ={
-                            (e) => {
-                                setEmail(e.target.value);
-                            }
-                        }
 
-                        value={email}
-                        
-                        placeholder = "    Enter your Email" className = "w-[300px] h-[50px] border border-[#e9e9e9] rounded-[20px] my-[20px]"/>
-                        <input
-                        
-                        onChange={
-    (e) => {
-        setPassword(e.target.value);
     }
-}
-                    
 
+    return(
+        <div className="w-full h-screen bg-[url('/login.jpg')] bg-center bg-cover flex  justify-evenly items-center">
+          <div className="w-[50%] h-full ">
+
+          </div>
+          <div className="w-[50%] h-full flex justify-center items-center">
+
+            <div className="w-[500px] h-[600px] backdrop-blur-md rounded-[20px] shadow-xl flex flex-col justify-center items-center">
+                <input 
+                
+                    onChange={
+                        (e)=>{
+                            setEmail(e.target.value)                        
+                        }
+                    }
+
+                    value={email}
+                
+                className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] my-[20px]" />
+                <input 
+                    onChange={
+                        (e)=>{
+                            setPassword(e.target.value)                        
+                        }
+                    }
                     value={password}
-                        
-                        placeholder = "    Enter your Password" type ="password" className = "w-[300px] h-[50px] border border-[#e9e9e9] rounded-[20px] my-[20px]"/>
-                        <button onClick = {handleLogin} className = "w-[300px] h-[50px] bg-[#e5c3c4] rounded-[20px] text-[20px] my-[20px] font-bold text-black cursor-pointer">Login</button>
-                    </div>
-                    </div>
-                    </div>
-        
-        
-        
-        )
+                type="password" className="w-[300px] h-[50px] border border-[#c3efe9] rounded-[20px] mb-[20px]" />
+                <button onClick={handleLogin}  className="w-[300px] cursor-pointer h-[50px] bg-[#c3efe9] rounded-[20px] my-[20px] text-[20px] font-bold text-white">Login</button>
+                <button onClick={googleLogin} className="w-[300px] cursor-pointer h-[50px] flex justify-center items-center bg-[#c3efe9] rounded-[20px] my-[20px] text-[20px] font-bold text-white" >
+                    <GrGoogle className="text-xl text-gray-600 cursor-pointer hover:text-gray-800" />
+                    <span className="text-gray-600 text-xl font-semibold">Login with Google</span>
+                </button>
+            </div>
 
-
-
+          </div>
+        </div>
+    )
 }
 
 
